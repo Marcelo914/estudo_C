1f245f94d604da434d3257b51f613db1afbf3415
marcelo de souza rodrigues <marcelo@pop-os.localdomain>
2023-04-18 18:43:20 -0300

    add

--- area_circulo.c
+++ area_circulo.c
@@ -5,8 +5,13 @@ int main()
   float raio, area;
   printf("Digite o diametro do raio\n");
   scanf("%f", &raio);
-
+  if (raio <= 0) {
+    printf("o raio precisa ser maior que 0\n");
+  }
+  else {
   area = 3.14*(raio * raio);
   printf("A area da circuferencia é %.2f\n", area);
+  }
   return 0;
+
 }
