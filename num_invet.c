02d7dec12f7a525690282ed57232e45f3cbf2e6c
marcelo de souza rodrigues <marcelo@pop-os.localdomain>
2023-04-18 18:30:42 -0300

    update

---
+++ num_invet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+  int num1, num2, num3;
+  int order[3];
+  printf("Digite 3 números\n");
+  scanf("%d%d%d", &num1, &num2, &num3);
+  printf ("%d %d %d\n", num3, num2, num1);
+
+  return 0;
+}
