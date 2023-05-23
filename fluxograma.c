1817ed575cc217954e0fb89db2e35167abfd8c41
marcelo914 <marcelosouza222244@gmail.com>
2023-05-23 19:32:02 -0300

    add

--- fluxograma.c
+++ fluxograma.c
@@ -2,24 +2,53 @@
 #include <stdlib.h>
 
 int main()
-{
-  int atender, cont, escolha;
-  printf("Ligação\n");
-  scanf("%d", &atender);
-  if (atender == 0){
-    while (cont < 2) {
-      printf("ligação\n");
-      scanf("%d", &atender);
-      cont ++;
+{ 
+  char interesses[10];
+  int atender, convite, bebida, tipo_bebida, n, result;
+  atender = 0;
+  n = 3;
+  while (atender != 1) {
+    printf("ligação\n");
+    printf("Deseja atender a ligação\n");
+    printf("1 = sim / 0 = não\n");
+    scanf("%d", &atender);
+    system("clear || cls");
+  }
+
+  if (atender == 1) {
+    printf("Convite para refeição\n");
+    printf("aceitar convite?\n");
+    scanf("%d", &convite);
+  }
+  if (convite == 0) {
+    printf("oferecer bebida\n");
+    scanf("%d", &bebida);
+  }
+  else {
+    printf("começar amizade\n");
+  }
+  if (bebida == 1) {
+    printf("café chá ou chocolate?\n");
+    scanf("%d", &tipo_bebida);
+    switch (tipo_bebida) {
+      case 1:
+      printf("café");
+      case 2:
+      printf("chá");
+      case 3:
+      printf("chocolate");
     }
   }
-  if (atender == 1) {
-    printf("Convidar parar a refeição\n");
-    printf("Digite 1 para aceitar e 0 para recusar\n");
-    scanf("%d", &escolha);
+  else {
+    do {
+      printf("quais seus interesses\n");
+      scanf("%s", interesses);
+      printf("gostou\n");
+      scanf("%d", &result);
+    } while(n <= 0 || result == 0);
   }
-  if (escolha == 1) {
-    
+  if (result == 1) {
+    printf("aee amizade\n");
   }
   return 0;
 }
--- teste.c
+++ teste.c
@@ -4,6 +4,5 @@ int main()
 {
   int i;
 
-
   return 0;
 }
