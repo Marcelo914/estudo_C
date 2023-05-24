8478a5585c5171cc2a6c8e6d639fa441d8403580
marcelo914 <marcelosouza222244@gmail.com>
2023-05-24 14:37:28 -0300

    add

--- user_name.c
+++ user_name.c
@@ -1,12 +1,22 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+
 int main(void)
 {
-  char string[200];
-  printf("Digite uma string em minusculo\n");
-  scanf("%s", string);
+  char nome[20];
+  int tamanho;
+
+  printf("Digite a string toda em minusculo: \n");
+  scanf("%s", nome);
   
-  
+  tamanho = strlen(nome);
+
+  printf("A string possui %d letras\n", tamanho);
+  for (int i = 0; i < tamanho; i++) {
+    printf("%c", toupper(nome[i]));
+  }
+  printf("\n");
   return 0;
 }
