a35d94e5131e61d2e64f74171365c5fcd6ecf2ea
marcelo914 <marcelosouza222244@gmail.com>
2023-05-24 13:46:12 -0300

    add

---
+++ user_name.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(void)
+{
+  char string[200];
+  printf("Digite uma string em minusculo\n");
+  scanf("%s", string);
+  
+  
+  return 0;
+}
