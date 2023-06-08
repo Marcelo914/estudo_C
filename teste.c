0df313f9d42e97db38530f70c9e31844a555d9a3
marcelo914 <marcelosouza222244@gmail.com>
2023-06-07 22:33:18 -0300

    autoadd

--- teste.c
+++ teste.c
@@ -1,28 +1,28 @@
 #include <stdio.h>
 int num1, num2, result, op;
- int marcelo (int num1, int num2) {
-	printf(" digite o priimeiro numero:\n");
-	scanf ("%d", &num1);
-	printf(" digite o segundo numero:\n");
-	scanf ("%d", &num2);
-	}
-		
- int main (void) {
-	
-	scanf("%d", &op);
-	
-	switch (op){
-	   case 1 :
-	     num1 + num2; 
-	     printf ("%d \n", result);
-	   case 2 :
-	       num1 - num2;
-	      printf ("%d\n", result);
-	   case 3 :
-        	 num1 * num2;
-	        printf ("%d\n,", result);
-	   case 4 : 
-	         num1 / num2;
-	         printf ("%d\n", result);
+int marcelo(int num1, int num2) {
+  printf(" digite o primeiro numero:\n");
+  scanf("%d", &num1);
+  printf(" digite o segundo numero:\n");
+  scanf("%d", &num2);
+}
+
+int main(void) {
+
+  scanf("%d", &op);
+
+  switch (op) {
+  case 1:
+    result = num1 + num2;
+    printf("%d \n", result);
+  case 2:
+    result = num1 - num2;
+    printf("%d\n", result);
+  case 3:
+    result = num1 *num2;
+    printf("%d\n,", result);
+  case 4:
+    result = num1 / num2;
+    printf("%d\n", result);
   }
-	}
+}
